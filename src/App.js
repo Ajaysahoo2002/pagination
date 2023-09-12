7193ed36dbb47722ce2c2b6207c8d7bf5ca50df9
unknown <Ajay Sahoo>
2023-09-12 15:24:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const API = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 12, 15, 16, 17, 18, 19, 20,21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
+  const [page, setPage] = useState(1);
+  const pageHandler = (selectedPage) => {
+    if (selectedPage >= 1 && selectedPage <= API.length && selectedPage!==page) 
+      {
+        setPage(selectedPage);
+        
+      }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pagination!</h1>
+
+      {/* Pagination Section */}
+
+      {API.slice(page * 5 - 5, page * 5).map((elem, id) => {
+        return <div id='number-display' key={id}>{elem}</div>
+      })}
+      {/* Page Navigation Buttons */}
+      <button type='button' id='back-button' disabled={page===1?true:false} name='button' onClick={() => { pageHandler(page - 1) }} >Prev Page</button>
+      <button type='button' id='next-button' name='button' disabled={page===6?true:false} onClick={() => { pageHandler(page + 1) }}>Next Page</button>
     </div>
   );
 }
